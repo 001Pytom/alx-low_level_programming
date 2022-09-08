91e0127e54f0ec44cecff4da300d01a332c3e81a
root <faatihat>
2022-09-08 16:07:24 -0700

    Write a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the size of various types
- *   * Return: 0 if exited properly, non-zero otherwise
- *    */
+ *  * main - size of various types
+ *
+ * description: prints the size
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
