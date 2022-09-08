750f61c630efa615f3e95807f8115dd9d535c334
root <faatihat>
2022-09-08 16:26:25 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints out last paart of a code to standard error
+ *
+ * Description: still the ame as the first description
+ * return: 1 if successful
+ */
+int main(void)
+{
+	write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
