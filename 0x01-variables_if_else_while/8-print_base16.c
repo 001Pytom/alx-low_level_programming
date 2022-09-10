1fe83870379532061623c52e68a4d06dc6488d04
root <faatihat>
2022-09-10 12:12:37 -0700

    Write a program that prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: its prints all 
+ * hexadecimalnumbers
+ * Return: 0 always
+ */
+int main(void)
+{
+
+	int i;
+	char alpha;
+
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
+	for (alpha = 'a'; alpha < 'g'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+
+	return (0);
+}
+
