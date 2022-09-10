4841e97b4fe2fecd277e92688611630d9c7e47e8
root <faatihat>
2022-09-10 08:30:30 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - my entry point
+ * @ letter: my character
+ *
+ * its prints outupper and loer case
+ * Return: 0 always
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; leter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
