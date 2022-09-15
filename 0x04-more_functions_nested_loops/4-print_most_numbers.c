f7bf642cadeb8e804d62a6e763447b1fb5821acf
root <faatihat>
2022-09-15 16:17:02 -0700

    i believ in numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,7 @@
 /**
  * print_most_numbers - prints 0 to 9 n=but not 2 and 4
  *
- * Description: prints numbers from 0 to 9 wthout
- * 2 and 4
+ * Description: prints numbers from 0 to 9 wthout2 and 4
  * Return: 0
  */
 void print_most_numbers(void)
