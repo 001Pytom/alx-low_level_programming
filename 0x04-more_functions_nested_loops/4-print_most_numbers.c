4c962e8c4354a09c0ae9a239e7cffb111cae9a6c
root <faatihat>
2022-09-15 16:22:24 -0700

    i believ in numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,14 +2,13 @@
 /**
  * print_most_numbers - prints 0 to 9 n=but not 2 and 4
  *
- * Description: prints numbers from 0 to 9 wthout2 and 4
  * Return: 0
  */
 void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c='0'; c<='9'; c++)
 	{
 		if (c!='4' && c!='2')
 		{
