4af5a60782993e9087e35ae9fc798e07f8ab4ae9
root <faatihat>
2022-09-15 15:44:33 -0700

    Write a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int _putchar(char c);
+int _putchar(char num);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
