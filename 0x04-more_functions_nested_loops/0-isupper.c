fae4b5180587b2aba7bdad13dc16e6b758d887fb
root <faatihat>
2022-09-15 15:07:19 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,8 +3,7 @@
  * _isupper - it checks for uppercase
  *
  * @c: the number to be checked
- * Return: 1 if c is upper
- * Return: 0 otheriswe
+ * Return: 1 if c is upper and 0 otherwise
  */
 int _isupper(int c)
 {
