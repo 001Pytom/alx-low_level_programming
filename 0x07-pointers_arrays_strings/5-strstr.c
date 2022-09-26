8e45d54db15422e082175a1b2e4b9ff21c96c16e
root <faatihat>
2022-09-26 06:51:00 -0700

    Write a function that locates a substring.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strsstr - find the first occurence of substring
+ * @haystack: string to work on
+ * @needle: sunstring
+ *
+ * Return: pointer to the first match or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, match;
+
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == *needle)
+		{
+			for (j = 1; needle[j] != '\0'; j++)
+			{
+				if (needle[j] != haystack[i + j])
+				{
+					match = 0;
+					break;
+				}
+				match = 1;
+			}
+			if (match)
+				return (haystack + i);
+		}
+	}
+	return (NULL)
+}
