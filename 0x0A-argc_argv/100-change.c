6594ab725b4e32282367e49254dcae8b8dd4022a
root <faatihat>
2022-10-02 02:29:42 -0700

    all the tasks

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name
+ * @argc: parameter
+ * @argv: array of a cmd listed
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int num, j, result = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			num -= coins[j];
+			result++;
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
