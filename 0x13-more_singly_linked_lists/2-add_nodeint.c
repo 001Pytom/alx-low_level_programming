fc980e329d833b0c01dbfbef1808e40f9a3976a5
faatihat <tomfaah@gmail.com>
2022-10-25 20:18:36 -0700

    done

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,13 +8,14 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *tp;
-
-	if (tp == NULL)
+	listint_t *new;
+	new = malloc(sizeof(listint_t));
+	if (!new)
 		return (NULL);
 
-	tp->n = n;
-	tp->next = *head;
-	*head = tp;
-	return (*head);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
